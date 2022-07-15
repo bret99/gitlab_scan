e103314971a5b815ab49c6119db8821722d00b7c
bret99 <64787351+bret99@users.noreply.github.com>
2022-07-15 10:39:05 +0000

    Update gitlab_scan.py

--- gitlab_scan.py
+++ gitlab_scan.py
@@ -14,11 +14,12 @@ from get_runners import get_runners
 from get_runner_info import get_runner_info
 from get_runner_jobs import get_runner_jobs
 from get_runners_IPs import get_runners_IPs
+from search import get_search
 
 def main_menu():
     try:
         print(
-            "\033[1;91m\nGitlab Scanner \033[1;00mmodules:\n\n[\033[1;91m1\033[1;00m] Get users\n\033[1;00m[\033[1;91m2\033[1;00m] Get user info\n\033[1;00m[\033[1;91m3\033[1;00m] Get users IPs\n\033[1;00m[\033[1;91m4\033[1;00m] Get namespaces\n\033[1;00m[\033[1;91m5\033[1;00m] Get namespace info\033[1;00m\n[\033[1;91m6\033[1;00m] Get projects\033[1;00m\n[\033[1;91m7\033[1;00m] Get project info\033[1;00m\n[\033[1;91m8\033[1;00m] Get project users\n\033[1;00m[\033[1;91m9\033[1;00m] Get project hooks\n\033[1;00m[\033[1;91m10\033[1;00m] Get hook info\n\033[1;00m[\033[1;91m11\033[1;00m] Get runners\n\033[1;00m[\033[1;91m12\033[1;00m] Get runner info\n\033[1;00m[\033[1;91m13\033[1;00m] Get runner jobs\n\033[1;00m[\033[1;91m14\033[1;00m] Get runners IPs\n\033[1;00m[\033[1;91m99\033[1;00m]\033[1;90m Exit\033[1;00m\n"
+            "\033[1;91m\nGitlab Scanner \033[1;00mmodules:\n\n[\033[1;91m1\033[1;00m] Get users\n\033[1;00m[\033[1;91m2\033[1;00m] Get user info\n\033[1;00m[\033[1;91m3\033[1;00m] Get users IPs\n\033[1;00m[\033[1;91m4\033[1;00m] Get namespaces\n\033[1;00m[\033[1;91m5\033[1;00m] Get namespace info\033[1;00m\n[\033[1;91m6\033[1;00m] Get projects\033[1;00m\n[\033[1;91m7\033[1;00m] Get project info\033[1;00m\n[\033[1;91m8\033[1;00m] Get project users\n\033[1;00m[\033[1;91m9\033[1;00m] Get project hooks\n\033[1;00m[\033[1;91m10\033[1;00m] Get hook info\n\033[1;00m[\033[1;91m11\033[1;00m] Get runners\n\033[1;00m[\033[1;91m12\033[1;00m] Get runner info\n\033[1;00m[\033[1;91m13\033[1;00m] Get runner jobs\n\033[1;00m[\033[1;91m14\033[1;00m] Get runners IPs\n\033[1;00m[\033[1;91m15\033[1;00m] Search\n\033[1;00m[\033[1;91m99\033[1;00m]\033[1;90m Exit\033[1;00m\n"
         )
         choose_module = input("Enter module number: ")
         if choose_module == "1":
@@ -62,6 +63,9 @@ def main_menu():
             main_menu()
         elif choose_module == "14":
             get_runners_IPs()
+            main_menu()
+        elif choose_module == "15":
+            get_search()
             main_menu()
         elif choose_module == "99":
             sys.exit()
