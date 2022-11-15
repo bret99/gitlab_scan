8c894a7c6f68a563ba9592bd9a04191ca140514f
bret99 <64787351+bret99@users.noreply.github.com>
2022-11-15 12:15:14 +0000

    Added ipapi provider

--- access_tokens.py
+++ access_tokens.py
@@ -2,3 +2,4 @@ gitlab_access_token = ""
 gitlab_server_address = "" # https://gitlab.example.com
 abuseipdb_token = "" # https://www.abuseipdb.com; 1000 per day, 30000 per month limited
 ipgeolocation_token = "" # https://ipgeolocation.io; 1000 per day, 30000 per month limited
+ipapi_token = "" # https://ipapi.com; 1000 per month limited
